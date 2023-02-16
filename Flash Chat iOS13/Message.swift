dbb2310d914c6ebcae47a44158ebb831ae291a7c
giorgi <tushi89@yahoo.com>
2023-02-16 14:41:38 +0400

    commit changes

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by MACINTOSHI on 09.02.23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
